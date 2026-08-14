f79a8752d960c7b667c19f23dfd4d9e49ea387be
Anees <aneespengad4447@gmail.com>
2026-08-14 16:00:20 +0530

    feat(about): add about us and vision mission sections
    - add AboutUsSection component to about page
    - add VisionMissionSection component to about page
    - install lucide-react dependency for icons

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,14 @@
 import AboutHero from "@/components/About/AboutHero";
+import AboutUsSection from "@/components/About/AboutUsSection";
+import VisionMissionSection from "@/components/About/VisionMissionSection";
 import React from "react";
 
 const page = () => {
   return (
     <main>
       <AboutHero />
+      <AboutUsSection />
+      <VisionMissionSection />
     </main>
   );
 };

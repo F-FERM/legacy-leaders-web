7295a5bbc3acd59bbdddbf50986c6940f4afe9b8
Anees <aneespengad4447@gmail.com>
2026-08-14 15:20:27 +0530

    feat(home): add hero and about sections to landing page
    - remove media query dark mode block from global styles
    - update navbar import to use named import and fix component spacing
    - integrate Hero and HomeAboutSection components into main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Hero from "@/components/Home/Hero";
+import HomeAboutSection from "@/components/Home/HomeAbout";
 import React from "react";
 
 const Home = () => {
-  return <main></main>;
+  return (
+    <main>
+      <Hero />
+      <HomeAboutSection/>
+    </main>
+  );
 };
 
 export default Home;

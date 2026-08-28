fd6eb894c634bcb2f06bdb68a297b1f581f40b1b
Anees <aneespengad4447@gmail.com>
2026-08-28 16:58:30 +0530

    feat(home): add why legacy leaders section
    - import and render WhyLegacyLeaders component
    - format self-closing tags for other home page components

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,18 @@ import Hero from "@/components/Home/Hero";
 import HomeAboutSection from "@/components/Home/HomeAbout";
 import RecentArticles from "@/components/Home/RecentArticles";
 import VisionMission from "@/components/Home/VisionMission";
+import WhyLegacyLeaders from "@/components/Home/WhyLegacyLeaders";
 import React from "react";
 
 const Home = () => {
   return (
     <main>
       <Hero />
-      <HomeAboutSection/>
-      <VisionMission/>
-      <CoreServices/>
-      <RecentArticles/>
+      <HomeAboutSection />
+      <VisionMission />
+      <CoreServices />
+      <WhyLegacyLeaders />
+      <RecentArticles />
     </main>
   );
 };
